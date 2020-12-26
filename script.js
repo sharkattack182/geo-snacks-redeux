316cf08e5cc2b4f7c7f1dd0d38bca6bb041a5ec4
Matthew Courtney <matthewdc182@gmail.com>
2020-12-25 20:33:24 -0500

    i am now returning a list of available cusines in an area

--- script.js
+++ script.js
@@ -3,5 +3,22 @@ $( document ).ready(function() {
 
     $.get("https://ipapi.co/json").then(function (response) {
         console.log(response);
+
+        // setting lat and long for the search query
+        var lat = response.latitude;
+        var lon = response.longitude;
+
+        // calling zomato api based only off the latitude and longitude at the moment
+        $.ajax({
+            headers: {
+            "user-key": "8717f09646df7022f6022fc3d15f3584"
+        },
+        method: "GET",
+        url: "https://developers.zomato.com/api/v2.1/cuisines?lat=" + lat + "&lon=" + lon
+    }).then(function(result) {
+        console.log(result)
+    })
+
+
     })
 })
