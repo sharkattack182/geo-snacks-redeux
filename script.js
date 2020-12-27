dc094f9f808db27d266bb2312649ead1d326fc18
Matthew Courtney <matthewdc182@gmail.com>
2020-12-26 20:11:49 -0500

    added an onclick function for the button and i also added the cuisine id as a data attribute that console logs appropriately whent he button is clicked

--- script.js
+++ script.js
@@ -17,11 +17,13 @@ $( document ).ready(function() {
         url: "https://developers.zomato.com/api/v2.1/cuisines?lat=" + lat + "&lon=" + lon
     }).then(function(result) {
         console.log(result)
+        // runs for loop over the resutls array then build and append a div for each result
         for (var i = 0; i < result.cuisines.length; i++) {
             var cuisine = result.cuisines[i];
-            var squareEl = $("<div>");
+            var squareEl = $("<button>");
             squareEl.text(cuisine.cuisine.cuisine_name);
             squareEl.attr("class", "col-lg-3 box " + cuisine.cuisine.cuisine_name);
+            squareEl.data("cuisine-id", cuisine.cuisine.cuisine_id)
             if(i < 3) {
                 $(".contain-items1").append(squareEl)
             } else if(i > 2 && i < 6) {
@@ -31,10 +33,16 @@ $( document ).ready(function() {
             } else {
                 $(".contain-items4").append(squareEl)
             }
-            
+
         }
-    })
 
-
+        // on click function for the buttons
+        $(".box").on("click", function(event) {
+            console.log("you clicked a box");
+            console.log($(this).data('cuisine-id'))
+        })
     })
+})
+
+    
 })
